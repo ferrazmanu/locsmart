200b8418fa26e00d29335a032edab3d832b22343
ferrazmanu <ferraz.manuela@hotmail.com>
2025-04-03 13:57:39 -0300

    style: tabela de grupos

--- src/app/(protected)/admin/groups/groups.constants.ts
+++ src/app/(protected)/admin/groups/groups.constants.ts
@@ -1,10 +1,10 @@
 import { ITableHeader } from "@/src/components/table/table.interfaces";
 
 const TABLE_HEADER: ITableHeader[] = [
-  { title: "Nome", width: "300px" },
+  { title: "Nome", width: "200px" },
   { title: "Cameras", width: "80px" },
   { title: "Usuários", width: "80px" },
-  { title: "Descrição", width: "200px" },
+  { title: "Descrição", width: "300px" },
 ];
 
 export { TABLE_HEADER };
