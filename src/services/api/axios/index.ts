72b4e6cf272f60c8d904f18cb136074f01f4a707
ferrazmanu <ferraz.manuela@hotmail.com>
2025-04-04 11:22:06 -0300

    feat: logout quando response for 401

--- src/services/api/axios/index.ts
+++ src/services/api/axios/index.ts
@@ -1,3 +1,4 @@
+import { deleteSession } from "@/src/app/lib/session";
 import { getLocalStorage } from "@/src/utils/storage";
 import axios from "axios";
 
@@ -18,6 +19,19 @@ api.interceptors.request.use(
     }
   },
   (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      deleteSession();
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default api;
