4e3a609914571160851688efb23ef3dc441b668e
ferrazmanu <ferraz.manuela@hotmail.com>
2025-04-08 17:12:43 -0300

    fix: meta scale

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,10 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
         <link rel="icon" href="/favicon.ico" />
       </head>
 
