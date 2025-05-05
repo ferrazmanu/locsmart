e9aa11126cb54bf33852a6f888e3d21864d5f848
ferrazmanu <ferraz.manuela@hotmail.com>
2025-05-05 10:36:58 -0300

    fix: muda nome chave env

--- src/contexts/dashboard/dashboard.context.tsx
+++ src/contexts/dashboard/dashboard.context.tsx
@@ -75,7 +75,7 @@ export const DashboardContextProvider: React.FC<{
   }, []);
 
   const getSetEnvironment = () => {
-    const envFile = `${process.env.NODE_ENV}`;
+    const envFile = `${process.env.NEXT_PUBLIC_ENV}`;
 
     console.log("envFile", envFile);
 
